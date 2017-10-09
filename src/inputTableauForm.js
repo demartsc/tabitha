390395f1273a95b8ab38bc71677ab82ce27fb1c0
Chris DeMartini <demartsc@Chriss-MacBook-Pro.local>
2017-10-09 14:01:01 -0700

    added resize so that frame will respond to the size of the loaded viz

--- src/inputTableauForm.js
+++ src/inputTableauForm.js
@@ -3,6 +3,7 @@ import propTypes from 'prop-types';
 import Tableau from 'tableau-api';
 import Speak from './Speak.js';
 import TableauReport from 'tableau-react';
+import underscore from 'underscore';
 
 class InputTableau extends React.Component {
   constructor(props) {
@@ -17,14 +18,16 @@ class InputTableau extends React.Component {
     this.handleButtonClick = this.handleButtonClick.bind(this);
     this.initTableau = this.initTableau.bind(this);
     this.tempURL = null;
+    this.width = 800; // default, although this gets overwritten in the initTableau function
+    this.height = 800; // default, although this gets overwritten in the initTableau function
   }
 
   initTableau() {
     const vizURL = this.state.url;
     const options = {
       hideTabs: true,
-      width: '700px',
-      height: '800px',
+      width: this.width,
+      height: this.height,
       onFirstInteractive: () => {
         const wrkbk = viz.getWorkbook();
         const activeSheet = viz.getWorkbook().getActiveSheet();
@@ -32,8 +35,19 @@ class InputTableau extends React.Component {
         const name = wrkbk.getName();
         const objs = activeSheet.getObjects();
         const pubSheets = wrkbk.getPublishedSheetsInfo();
-        //console.log(objs);
+        console.log(objs);
+        console.log(objs[2].getObjectType());
         //console.log(sheets);
+
+        // need to check what happens with automatic sized workbooks...
+        this.width = activeSheet.getSize().maxSize.width;
+        this.height = activeSheet.getSize().maxSize.height;
+
+        // this will set the frame size the maximum allowed by the viz
+        // need to vet whether this will be a problem with automatic vizzes however
+        // see note herein for dashboards as well...
+        // https://onlinehelp.tableau.com/current/api/js_api/en-us/JavaScriptAPI/js_api_sample_resize.html
+        viz.setFrameSize(this.width, this.height + 100);
 
         for (let i = 0; i < sheets.length; i++) {
           sheets[i].getFiltersAsync().then(f => {
@@ -41,7 +55,7 @@ class InputTableau extends React.Component {
           });
         }
 
-        if (pubSheets.length == 1) {
+        if (pubSheets.length === 1) {
           this.setState({
             speakText:
               'This workbook is named ' +
@@ -67,7 +81,7 @@ class InputTableau extends React.Component {
           });
           // if the user has provided the description parameter this will read it back to the user, otherwise it will do nothing.
           for (let j = 0; j < t.length; j++) {
-            if (t[j].getName().toUpperCase() == 'DESCRIPTION') {
+            if (t[j].getName().toUpperCase() === 'DESCRIPTION') {
               this.setState({
                 speakText: 'The author has provided the following description. '
               });
@@ -76,13 +90,16 @@ class InputTableau extends React.Component {
               });
             }
           }
-          //console.log(t);
+          console.log(t);
         });
       }
     };
 
+    //initiate the viz
     let viz = new Tableau.Viz(this.container, vizURL, options);
-    if (this.state.speakText == 'Thanks! I am updating your workbook now.') {
+
+    //resize the viz based on the values
+    if (this.state.speakText === 'Thanks! I am updating your workbook now.') {
       this.setState({
         viz: viz,
         speakText: ''
@@ -113,7 +130,7 @@ class InputTableau extends React.Component {
   // one problem is that we are changing state a lot we only want this to be called on viz update.
   componentDidUpdate(prevProps, prevState) {
     console.log('updated');
-    if (prevState.url != this.state.url) {
+    if (prevState.url !== this.state.url) {
       this.initTableau(); // we are just using state, so don't need to pass anything
     }
   }
@@ -121,9 +138,9 @@ class InputTableau extends React.Component {
   componentWillUpdate(nextProps, nextState) {
     console.log('will update');
 
-    if (this.state.viz && nextState.url != this.state.url) {
+    if (this.state.viz && nextState.url !== this.state.url) {
       this.state.viz.dispose();
-      this.state.viz = null;
+      //this.state.viz = null;
     }
   }
 
